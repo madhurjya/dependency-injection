c3d4357f0961131f3bf8b9e253e10ab7ce67f788
Subhajit Das <subhajitdas86@gmail.com>
2016-06-24 00:26:22 +0530

    Basic dependency Resolution
    With constructor argument resolution.

--- src/demo.js
+++ src/demo.js
@@ -1,1 +1,43 @@
 import {DependencyContainer as DI} from './dependency';
+
+class SqlDataService {
+    constructor() {
+        this._name = 'Sql';
+    }
+}
+
+class OracleDataService {
+    constructor() {
+        this._name = 'Oracle';
+    }
+}
+
+class MyCollection {
+    constructor(size, dataService) {
+        this._size = size;
+        this._dataService = dataService;
+    }
+    get maxSize() {
+        return this._maxSize;
+    }
+    set maxSize(value) {
+        this._maxSize = value;
+    }
+}
+
+DI.register('dataservice').asType(SqlDataService);
+DI.register('dataservice').asType(SqlDataService, 'sql');
+DI.register('dataservice').asType(OracleDataService, 'oracle');
+
+DI.register('collection').asType(Map);
+DI.register('collection').asType(Set, 'set');
+DI.register('collection')
+    .asType(MyCollection, 'my')
+    .withConstructor(ctor => {
+        ctor.param('size').asValue(5);
+        ctor.param('dataService').asDependency('dataservice');
+    });
+
+const resolvedInstance = DI.resolve('collection', 'my');
+
+console.log(resolvedInstance);
--- src/dependency.js
+++ src/dependency.js
@@ -1,6 +1,133 @@
+const _registrations = new Map();
+
 class DependencyContainer {
+    static register(name) {
+        if (!_registrations.has(name)) {
+            _registrations.set(name, new DependencyMapping());
+        }
+        return _registrations.get(name);
+    }
+    static resolve(registrationName, mappingName) {
+        if (!_registrations.has(registrationName)) {
+            throw new Error(`${registrationName} is not registered.`);
+        }
+        const dependencyMapping = _registrations.get(registrationName);
+        if (!dependencyMapping._mappings.has(mappingName)) {
+            throw new Error(`Mapping ${mappingName} is not registered.`);
+        }
+        const dependencyDetails = dependencyMapping._mappings.get(mappingName);
+        return dependencyDetails._resolve();
+    }
+}
+
+class DependencyMapping {
     constructor() {
-       this._registrations = new Map();
+        this._mappings = new Map();
+    }
+    asType(type, mappingName) {
+        if (this._mappings.has(mappingName)) {
+            throw new Error(`${mappingName || 'default'} mapping is already registered.`);
+        }
+        const dependencyDetails = new DependencyDetails(type);
+        this._mappings.set(mappingName, dependencyDetails);
+        return dependencyDetails;
+    }
+}
+
+class DependencyDetails {
+    constructor(type) {
+        this._type = type;
+        this._resolveWithConstructor = false;
+        this._constructorParameterMapper = null;
+
+        this._resolvedInstance = null;
+    }
+    withConstructor(parameterMapperFunc) {
+        if (this._resolveWithConstructor === true) {
+            throw new Error('Constructor is already configured.');
+        }
+        this._resolveWithConstructor = true;
+        this._constructorParameterMapper = new ParameterMapper(getFunctionArguments(this._type));
+        if (isFunction(parameterMapperFunc)) {
+            parameterMapperFunc(this._constructorParameterMapper);
+        }
+        return this._constructorParameterMapper;
+    }
+
+    _resolve() {
+        if (this._resolveWithConstructor) {
+            const constructorArguments = this._constructorParameterMapper._resolve();
+            this._resolvedInstance = new this._type(...constructorArguments);
+        } else {
+            this._resolvedInstance = new this._type();
+        }
+        return this._resolvedInstance;
+    }
+}
+
+class ParameterMapper {
+    constructor(parameterNames) {
+        this._parameterMap = new Map(
+            parameterNames
+                .map(param => [param, new ParameterDetails(param)])
+        );
+    }
+    param(name) {
+        if (!this._parameterMap.has(name)) {
+            throw new Error(`Argument "${name}" was not detected.`);
+        }
+        return this._parameterMap.get(name);
+    }
+
+    _resolve() {
+        return [...this._parameterMap.values()].map(param => param._resolve());
+    }
+}
+
+class ParameterDetails {
+    constructor(argumentName) {
+        this._name = argumentName;
+
+        this._resolveAsValue = false;
+        this._valueToResolve = null;
+
+        this._resolveAsDependency = false;
+        this._registrationNameToResolve = null;
+        this._mappingNameToResolve = null;
+    }
+    asValue(valueToResolve) {
+        this._resolveAsValue = true;
+        this._valueToResolve = valueToResolve;
+    }
+    asDependency(registrationName, mappingName) {
+        this._resolveAsDependency = true;
+        this._registrationNameToResolve = registrationName;
+        this._mappingNameToResolve = mappingName;
+    }
+
+    _resolve() {
+        if (this._resolveAsValue) {
+            return this._valueToResolve;
+        } else if (this._resolveAsDependency) {
+            return DependencyContainer.resolve(this._registrationNameToResolve,
+                this._mappingNameToResolve);
+        }
+    }
+}
+
+function isFunction(func) {
+    return typeof func === 'function';
+}
+
+function getFunctionArguments(func) {
+    const paramExtract = func.toString()
+        .match(/\s*function[^\(]*\(([^\)]*)\)/);
+    if (paramExtract && paramExtract.length === 2) {
+        return paramExtract[1]
+            .split(',')
+            .map(param => param.replace(/\/\/.*?[\r\n]|\/\*(?:.|[\r\n])*?\*\//g, '').trim())
+    } else {
+        throw new Error('Unable to fetch parameter details.');
     }
 }
 
